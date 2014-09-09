62564b2a8ab1e00311a0f6873bc1adf478ac3fae
pourlespme <bprince@pourlespme.com>
2014-09-08 20:50:56 -0400

    update case error README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 README for git test
+Changement au test le 6 sept. 2014
+
---
+++ projet1_sio_2109/bin/projet1_sio_2109.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("Hello, World!");
+  
+  String prenom = 'Bernard';
+  String nom = 'Prince';
+  print ('${prenom}, ${nom}');
+  }
